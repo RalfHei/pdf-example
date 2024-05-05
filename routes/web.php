38edac844f6eb92bd9fc2142ac7d360068ae4f76
ralf <ralf@veebisepad.ee>
2024-05-05 12:42:26 +0300

    run command as process

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-use App\Jobs\GeneratePdf;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,9 +13,28 @@ Route::get('/', function () {
 
 Route::get('/pdf-example-1', function () {
 
-    $pdf = GeneratePdf::dispatchSync();
+    $key = Str::random(40);
+    $php_bin = config('app.php_binary');
 
-    return response()->streamDownload(function () use ($pdf) {
-        echo $pdf;
-    }, 'example.pdf');
+    $process = Process::path(base_path())->start("$php_bin artisan app:generate-pdf $key");
+
+    while ($process->running()) {
+        Log::info($process->latestOutput());
+        Log::info($process->latestErrorOutput());
+    }
+
+    $result = $process->wait();
+
+    if ($result->successful()) {
+        Artisan::call('app:generate-pdf', ['key' => $key]);
+
+        return response()->streamDownload(function () use ($key) {
+            echo Cache::pull($key);
+        }, 'example.pdf');
+    }
+
+    if ($result->failed()) {
+        abort(500);
+    }
+
 })->name('print');
