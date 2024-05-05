f63972eb98c1fad57f75ac6f31f162c48814e285
ralf <ralf@veebisepad.ee>
2024-05-05 13:21:05 +0300

    dispatch pdf to queue

--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Process;
+use App\Jobs\GeneratePdf;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Str;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
 Route::get('/pdf-example-1', function () {
-
-    $key = Str::random(40);
-    $php_bin = config('app.php_binary');
-
-    $process = Process::path(base_path())->start("$php_bin artisan app:generate-pdf $key");
-
-    while ($process->running()) {
-        Log::info($process->latestOutput());
-        Log::info($process->latestErrorOutput());
-    }
-
-    $result = $process->wait();
-
-    if ($result->successful()) {
-        Artisan::call('app:generate-pdf', ['key' => $key]);
-
-        return response()->streamDownload(function () use ($key) {
-            echo Cache::pull($key);
-        }, 'example.pdf');
-    }
-
-    if ($result->failed()) {
-        abort(500);
-    }
+    GeneratePdf::dispatch();
 
 })->name('print');
